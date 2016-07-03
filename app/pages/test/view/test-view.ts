a809b49747f97b4f90243fab962701cc48e8ef7e
Denys Bogdanov <denys@18909.com>
2016-07-03 09:35:59 +0800

    Full working alfa quiz system

--- app/pages/test/view/test-view.ts
+++ app/pages/test/view/test-view.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { DB } from '../../../providers/db/db';
-import {Notification} from '../../../providers/notification/notification';
 
 
 @Component({
   templateUrl: 'build/pages/test/view/test-view.html',
-  providers: [DB, Notification]
 
 })
 @Component({
@@ -21,49 +18,59 @@ export class TestViewPage {
   score : number;
 
   question : any;
-  options : any;
+  options : any = [];
   answer : any;
   answerMode : boolean;
 
-  qestions: any;
+  qestions: any = [];
   title: string;
 
-  constructor(private nav: NavController, private db: DB, public notif: Notification, private navParams: NavParams) {
-    this.qestions = this.navParams.get('test').list;
-    this.title = this.navParams.get('test').hr;
-    this.id = 1;
-    this.quizOver = false;
-    this.inProgress = true;
-    this.getQuestion();
+  constructor(private nav: NavController,  private navParams: NavParams) {
+    let answ = this.navParams.get('test');
+    for(let i = 1; i <  answ.list.length; i++) {
+      this.qestions.push(answ.list[i]);
+    }
+    this.title = this.navParams.get('test').title;
     this.reset();
+  }
+
+  start(){
+    this.inProgress = true;
   }
 
   reset() {
     this.inProgress = false;
+    this.quizOver = false;
     this.score = 0;
+    this.id = 0;
+    this.getQuestion();
   }
 
   getQuestion() {
-    let q = this.qestions(this.id);
+    let q = this.qestions[this.id];
     if(q) {
       this.question = q.q;
-      this.options = q.v;
+      this.options = [];
+      for(let i = 1; i <  q.v.length; i++) {
+        this.options.push(q.v[i]);
+      }
       this.answer = q.a;
       this.answerMode = true;
     } else {
       this.quizOver = true;
     }
+
   }
 
-  checkAnswer(opt) {;
-    if(opt == this.options[this.answer]) {
+  checkAnswer(e, opt) {
+    if(opt === this.options[this.answer-1]) {
       this.score++;
       this.correctAns = true;
     } else {
       this.correctAns = false;
     }
     this.answerMode = false;
-  };
+  }
 
   nextQuestion() {
     this.id++;
